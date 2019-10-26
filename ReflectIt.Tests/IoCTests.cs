1d7bc8ebf38cdab29bbd2642ced6cbe99bbac3d8
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-10-26 20:51:39 +0200

    Module 6 Working With Constructors

--- ReflectIt.Tests/IoCTests.cs
+++ ReflectIt.Tests/IoCTests.cs
@@ -15,5 +15,19 @@ namespace ReflectIt.Tests
 
             Assert.AreEqual(typeof(SqlServerLogger), logger.GetType());
         }
+
+        [Test]
+        public void Can_Resolve_Types_Without_Default_Ctor()
+        {
+            Container ioc = new Container();
+
+            ioc.For<ILogger>().Use<SqlServerLogger>();
+            ioc.For<IRepository<Employee>>().Use<SqlRepository<Employee>>();
+
+
+            var repository = ioc.Resolve <IRepository<Employee>>();
+
+            Assert.AreEqual(typeof(SqlRepository<Employee>), repository.GetType());
+        }
     }
 }
