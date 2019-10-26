a5a8f41907b92bb0f9990a76474d02b640e0e5af
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-10-26 19:41:34 +0200

    Module 6 Instantiating Generic Types

---
+++ ReflectIt/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReflectIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employeeList = CreateCollection(typeof(List<>), typeof(Employee));
+            Console.WriteLine(employeeList.GetType().Name);
+            Console.WriteLine(employeeList.GetType().FullName);
+            var genericArguments = employeeList.GetType().GenericTypeArguments;
+
+            foreach (var arg in genericArguments)
+            {
+                Console.WriteLine("[{0}]", arg.Name);
+            }
+            Console.WriteLine();
+        }
+
+        private static object CreateCollection(Type collectionType, Type itemType)
+        {
+            var closedType = collectionType.MakeGenericType(itemType);
+            return Activator.CreateInstance(closedType);
+        }
+    }
+
+    public class Employee
+    {
+        public string Name { get; set; }
+    }
+}
